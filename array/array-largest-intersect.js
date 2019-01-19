a6587f6aa09ea2b126760532e927a8bff724282e
yareally10 <teny10@gmail.com>
2019-01-19 13:19:53 -0600

    add algorithm explanation

--- array/array-largest-intersect.js
+++ array/array-largest-intersect.js
@@ -1,19 +1,22 @@
-function binarySearch(arr, start, end, val) {
-    if(start <= end) {
-        var mid = start + Math.floor((end - start) / 2);
-        if(val == arr[mid]) {
-            return mid;
-        }
-        if(val < arr[mid]) {
-            return binarySearch(arr, start, mid - 1, val);
-        } else {
-            return binarySearch(arr, mid + 1, end, val);
-        }
-    }
-    return -1;
-}
+/*
+Given two sorted arrays, find biggest consecutive intersect between them
 
-//find biggest consecutive intersect between two sorted arrays
+
+Algorithm:
+Create two counters, one of each array.
+Loop through both arrays from the beginning, until the end for either array is reached.
+If current element for both arrays are equal, update current intersect, 
+and max intersect if necessary.
+Otherwise, increment the counter of the array with the smaller element, 
+reset the current intersect.
+Return largest intersect once entire array is looped.
+
+Test:
+findLargestIntersect(
+    [1, 2, 3, 4, 5, 7, 8, 9, 10, 12, 13, 14],
+    [2, 3, 4, 6, 7, 8, 9, 10, 11, 12]
+);
+*/
 
 function findLargestIntersect(arr1, arr2) {
     var i = 0,
@@ -23,7 +26,7 @@ function findLargestIntersect(arr1, arr2) {
         currIntersect = [],
         largestIntersect = [];
 
-    while(j < len2 && i < len1) {
+    while(i < len1 && j < len2) {
         if(arr1[i] == arr2[j]) {
             currIntersect.push(arr1[i]);
             largestIntersect = currIntersect.length > largestIntersect.length ? currIntersect : largestIntersect;
